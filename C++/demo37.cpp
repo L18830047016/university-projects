97073e4843a771ec7b35f5dcd2095d38555ce961
lst <3235244743@qq.com>
2026-05-24 10:58:03 +0800

    通过mutex互斥量（锁）解决多线程共享数据的安全问题

---
+++ C++/demo37.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <windows.h>
+using namespace std;
+
+//共享资源
+int ticket =10;
+int num=0;
+mutex mtx;//全局锁
+
+//售票线程执行体
+void sell_ticket(const string &name){
+    while(1){
+        mtx.lock();
+        if(ticket<=0){
+            mtx.unlock();
+            break;
+        }
+        ticket--;//模拟售出票
+        num++;
+        printf("%s买到了第%d张票，剩余%d张票\n",name.c_str(),num,ticket);
+        mtx.unlock();
+        Sleep(20);
+    }
+}
+
+int main(void){
+  thread t1(sell_ticket,"张三");
+  thread t2(sell_ticket,"李四");
+  thread t3(sell_ticket,"王五");
+  t1.join();
+  t2.join();
+  t3.join();
+
+  printf("售票结束\n");
+  return 0;
+}
